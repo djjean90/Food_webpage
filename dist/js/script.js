09156458b688db40a61714c1ed03be7bf7938024
djjean <djjean90@gmail.com>
2021-08-06 09:09:46 +0300

    js update

--- dist/js/script.js
+++ dist/js/script.js
@@ -296,3 +296,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }, 4000);
   }
 });
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
